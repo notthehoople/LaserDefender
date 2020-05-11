1126d90ee63707c2d9860a59219b0ff0b1b36d7b
Mark Bradshaw <notthehoople@googlemail.com>
2020-05-11 09:48:52 +0100

    Added Music and Singleton

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        SetUpSingleton();
+    }
+
+    private void SetUpSingleton()
+    {
+        // Get the Type of the object we are (e.g. MusicPlayer)
+        // This is a reusable singleton example
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            // If there is already a Music Player then get rid of the new one
+            Destroy(gameObject);
+        }
+        else
+        {
+            // If we're the first then don't destroy us
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
